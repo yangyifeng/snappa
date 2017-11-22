a308385658bc87ccfefe0681c7e283df1bb1804d
yang yifeng <yyf44@qq.com>
2017-11-22 12:42:25 +0800

    add demo

--- src/js/common.js
+++
@@ -1,7 +0,0 @@
-import avalon from "../vendor/avalon/avalon.mobile.shim";
-import $ from "../vendor/jquery/jquery-1.11.2";
-import yz from "./libs/yz";
-
-window.avalon = avalon;
-window.$ = window.jQuery = $;
-window.$$ = yz;
--- src/js/libs/yz.drag.done.js
+++
@@ -1,7 +0,0 @@
-//拖动，修改尺寸，旋转之后执行的操作
-function dragDone(element) {
-    //console.log(element)
-    //保存
-}
-
-window.dragDone = dragDone;
